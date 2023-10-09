43a5183e85432c65ade896ee674c3f4a1617871d
Argentfire121 <Technolancer121@gmail.com>
2023-10-08 22:36:45 -0700

    its a mess
    door

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator doorAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorAnim.SetBool("isOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAnim.SetBool("isOpening", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+}
---
+++ Assets/Scripts/DoorController2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController2 : MonoBehaviour
+{
+    Animator doorAnim;
+    public GameObject intIcon, DoorRight, DoorLeft;
+    public float openTime;
+
+    // Start is called before the first frame update
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intIcon.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                intIcon.SetActive(false);
+                doorAnim.SetBool("isOpening", true);
+                StartCoroutine(closeDoor());
+            }
+        }
+    }
+
+    IEnumerator closeDoor()
+    {
+        yield return new WaitForSeconds(openTime);
+        doorAnim.SetBool("isOpening", false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intIcon.SetActive(false);
+        }
+    }
+}
