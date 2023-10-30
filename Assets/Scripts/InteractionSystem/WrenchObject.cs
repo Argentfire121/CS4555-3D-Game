f9985fdc9952d25dcab8c080ca9ca4f91c1c8b38
SV-Dev768 <s.valadezl.0001@gmail.com>
2023-10-29 22:21:49 -0700

    Teleport and Interaction System
    Added the building blocks to make a teleporting and interaction system

---
+++ Assets/Scripts/InteractionSystem/WrenchObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrenchObject : MonoBehaviour, Interactable
+{
+
+    [SerializeField] private string _prompt;
+
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        Debug.Log(message:"Wrenched interacted with");
+        return true;
+    }
+}
